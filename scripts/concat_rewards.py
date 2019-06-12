a4d0edc8f811ae00262c7b7b1ef01e80988a01c7
camiloromers <48441162+camiloromers@users.noreply.github.com>
2019-06-12 16:05:31 +0200

    Modify the path to be compat with glob

--- scripts/concat_rewards.py
+++ scripts/concat_rewards.py
@@ -1,21 +1,20 @@
 import numpy as np
 import pandas as pd
-import glob
+from glob import glob
 import json
 import sys, os
 
 path_results = sys.argv[1]
-files_path = os.path.join(path_results, action_id_*.json)
+files_path = os.path.abspath(path_results)
 
+path = str(files_path) + '/'+'action_id_*.json'
 df = pd.DataFrame()
-for json_f in glob(files_path):
+for json_f in glob(path):
     with open(json_f) as f:
         config_data = json.load(f)
         config_df = pd.DataFrame(config_data['results']['rewards'], index=config_data['results']['datetimes'])
         config_df.columns = ['rewards_'+str(config_data['action_index'])]
         df = pd.concat([df ,config_df], axis=1)
 
-# df.head()
 df.to_csv('concat_rewards.csv')
-
 
